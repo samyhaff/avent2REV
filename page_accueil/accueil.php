5fa6722401ebcfb4b7c4b2ebe5a290e681f4632f
samyhaff <haffoudhisamy@gmail.com>
2020-11-22 15:45:01 +0100

    fixed borders

--- page_accueil/accueil.php
+++ page_accueil/accueil.php
@@ -1,6 +1,6 @@
 <?php 
     $date = 1;
-    $positions = array(array(200, 328), array(200, 328),
+    $positions = array(array(150, 300), array(200, 328),
         array(200, 328), array(200, 328), array(200, 328),
         array(200, 328), array(200, 328), array(200, 328),
         array(200, 328), array(200, 328), array(200, 328), 
@@ -16,19 +16,16 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" type="text/css"/>
         <title> Calendrier de l'Avent</title>
     </head>
 
     <body>
-        <figure id="fond">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1200 808" >
-                <image width="1200", height="800" xlink:href="images/fond.jpg">
-                </image>
                 <a xlink:href=<?php echo $type_calendrier;?>>
                 <image x=<?php echo $positions[$date - 1][0];?> y=<?php echo $positions[$date - 1][1];?> width="100" xlink:href="images/mongolfiere_rouge.png">
                     </image>
                 </a>
             </svg>
-        </figure>
     </body>
 </html>
